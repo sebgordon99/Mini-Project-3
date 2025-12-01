fa266cdb6a6f283f2de7fbb99b84b19748de5ea3
Sebastian Gordon <sebgordon99@gmail.com>
2025-12-01 19:48:42 +1100

    endpoints

--- mongodb-miniproject-app/microservices/coingeckoService.js
+++ mongodb-miniproject-app/microservices/coingeckoService.js
@@ -4,8 +4,8 @@ const fetchCoinData = async (api_id) => {
   const url = `https://api.coingecko.com/api/v3/coins/${api_id}`;
   const { data } = await axios.get(url);
   return {
-    current_price: data.market_data.current_price.usd,
-    market_cap: data.market_data.market_cap.usd
+    current_price: data.market_data.current_price.aud,
+    market_cap: data.market_data.market_cap.aud
   };
 };
 
